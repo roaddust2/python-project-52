4b2e0f78395863c2fb0a9d9e4b676f82309efedb
roaddust2 <roaddust2@pop-os.localdomain>
2023-02-25 17:21:22 +0400

    Add delete users

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from task_manager.users.views import (
     UsersCreateView,
     UsersListView,
-    UsersUpdateView
+    UsersUpdateView,
+    UserDeleteView
 )
 
 
 urlpatterns = [
     path('', UsersListView.as_view(), name='users_index'),
     path('<int:pk>/update/', UsersUpdateView.as_view(), name='users_update'),
-    # path('<int:pk>/delete/', UsersSignUpView.as_view(), name='users_delete'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='users_delete'),
     path('create/', UsersCreateView.as_view(), name='users_signup'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.contrib import messages
 from task_manager.users.forms import CustomUserCreationForm
@@ -12,6 +12,7 @@ from django.views.generic import (
     ListView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
 
 
@@ -25,7 +26,9 @@ class UsersListView(ListView):
 class UsersCreateView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'users/signup.html'
-    success_url = 'login'
+
+    def get_success_url(self):
+        return reverse('login')
 
 
 class UsersUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -37,6 +40,22 @@ class UsersUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         obj = self.get_object()
         return obj == self.request.user
 
+    def handle_no_permission(self):
+        messages.add_message(self.request, messages.ERROR, _('UsersUpdateError'), extra_tags='danger')
+        return redirect('users_index')
+
+    def get_success_url(self):
+        return reverse('users_index')
+
+
+class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = User
+    template_name = 'users/delete.html'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj == self.request.user
+    
     def handle_no_permission(self):
         messages.add_message(self.request, messages.ERROR, _('UsersUpdateError'), extra_tags='danger')
         return redirect('users_index')
