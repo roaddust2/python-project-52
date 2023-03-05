64ab92ddf476ab51cb83a5785e9baa0f7ce19a9e
roaddust2 <roaddust2@yahoo.com>
2023-03-05 19:27:39 +0400

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -95,13 +95,14 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
+db = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db)
 
 # Autentification
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth
